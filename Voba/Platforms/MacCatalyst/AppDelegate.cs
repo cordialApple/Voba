371cc5f514cf6d1db4174c241032fcd0192c4044
Andre iuB <andrebui2001@gmail.com>
2026-03-01 10:58:03 -0600

    Replace old project with new version .NET9

--- Voba/Platforms/MacCatalyst/AppDelegate.cs
+++ Voba/Platforms/MacCatalyst/AppDelegate.cs
@@ -1,9 +1,10 @@
 ﻿using Foundation;
 
-namespace Voba;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
+namespace Voba
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
 }
