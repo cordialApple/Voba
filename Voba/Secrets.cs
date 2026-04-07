f6552d0ca205c21e091bdb75eddf3519c4551508
Brandon Randle <randlebrandon5@gmail.com>
2026-04-07 01:07:53 -0500

    chore(git): ignore local secrets and remove build artifacts

---
+++ Voba/Secrets.cs
@@ -0,0 +1,9 @@
+namespace Voba
+{
+    internal static class Secrets
+    {
+        public const string MongoConnectionString = "YOUR_CONNECTION_STRING_HERE";
+        public const string MongoDatabaseName = "voba";
+    }     
+}
+ 
