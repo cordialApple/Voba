5d54100d790dfd5eb5302d072900879b99dd4c44
Brandon Randle <randlebrandon5@gmail.com>
2026-04-07 01:08:16 -0500

    feat(db): add MongoDB client and dependency

---
+++ Voba/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+
+namespace Voba
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+// MongoDB Client Setup
+             var mongoClient = new MongoClient(Secrets.MongoConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(Secrets.MongoDatabaseName);
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
