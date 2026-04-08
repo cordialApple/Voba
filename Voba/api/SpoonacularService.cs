2677da8ef8422617de8c51d4f67b579edabd6ceb
Vathana <thava1206@gmail.com>
2026-04-08 14:27:34 -0500

    add spoonacular SDK and confirm successful build

---
+++ Voba/api/SpoonacularService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using spoonacular.Api;
+using spoonacular.Client;
+using spoonacular.Model;
+
+namespace Voba.api
+{
+    internal class SpoonacularService
+    {
+    }
+}
