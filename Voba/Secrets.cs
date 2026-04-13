565c09d51d00ca6f48d399b47fb7d051c1853e56
Brandon Randle <randlebrandon5@gmail.com>
2026-04-13 09:59:47 -0500

    chore(git): rename credential template from Secret.cs to Secret.examples

--- Voba/Secrets.cs
+++
@@ -1,9 +0,0 @@
-namespace Voba
-{
-    internal static class Secrets
-    {
-        public const string MongoConnectionString = "YOUR_CONNECTION_STRING_HERE";
-        public const string MongoDatabaseName = "voba";
-    }     
-}
- 
