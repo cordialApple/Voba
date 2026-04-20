9528c1fba80512c4a071c28ab9ffe6dea1606e12
Andre Bui <andrebui2001@gmail.com>
2026-04-20 18:59:24 -0500

    fix gitignore

---
+++ SpoonacularSDK/spoonacular/ApiSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spoonacular
+{
+    /// <summary>
+    /// Holds the global configuration and secrets for external API services.
+    /// </summary>
+    public static class ApiSettings
+    {
+        /// <summary>
+        /// The API key required to authenticate requests to Spoonacular.
+        /// </summary>
+        public static string SpoonacularApiKey { get; set; } = "0f5055e31d424cc9bf30775ed374468f";
+    }
+}
