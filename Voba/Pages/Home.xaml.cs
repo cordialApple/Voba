fe390814823c10d5da66ae5f03cb5a0d195d596b
BrandonJ0113 <76008178+BrandonJ0113@users.noreply.github.com>
2026-04-21 00:09:48 -0500

    Add navigation methods for new pages

--- Voba/Pages/Home.xaml.cs
+++ Voba/Pages/Home.xaml.cs
@@ -1,9 +1,19 @@
 namespace Voba.Pages;
-
+ 
 public partial class Home : ContentPage
 {
     public Home()
     {
         InitializeComponent();
+    }
+ 
+    private async void OnCreateNewTapped(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("Forum");
+    }
+ 
+    private async void OnSavedTapped(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("SavedRecipes");
     }
 }
